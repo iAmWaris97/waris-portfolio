14805a5dbf5f3f4be9de43dac92902db2be96257
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-07-26 18:11:24 -0300

    Fix the disappear efect when clik in 'about' and 'contact'

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
-const navItem = document.querySelector('.nav-item');
+const navItem1 = document.querySelector('.waris1');
+const navItem2 = document.querySelector('.waris2');
+const navItem3 = document.querySelector('.waris3');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
-navItem.addEventListener('click', () => {
+navItem1.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+navItem2.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+navItem3.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
