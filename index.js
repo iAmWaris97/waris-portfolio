bf298a63ecd80200516bca75f495f99648955f52
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-07-27 01:53:58 +0430

    modify class names and fix showing mobile menu in desktop mode

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
-const navItem1 = document.querySelector('.waris1');
-const navItem2 = document.querySelector('.waris2');
-const navItem3 = document.querySelector('.waris3');
+const navItem1 = document.querySelector('.item1');
+const navItem2 = document.querySelector('.item2');
+const navItem3 = document.querySelector('.item3');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
