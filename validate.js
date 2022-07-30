ed3f104904c35213df16794c4ff9172be9dd0096
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-07-30 22:30:12 +0430

    add scripts to validate the email input in form

---
+++ validate.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('.contact-form');
+const email = document.getElementById('email');
+const errorMessage = document.querySelector('.error');
+
+function checkEmail(input) {
+  const lower = input.toLowerCase();
+
+  if (input !== lower) {
+    errorMessage.innerText = 'Please enter your email in lower case only!';
+    errorMessage.className = 'alert error';
+    return false;
+  }
+  if (input === lower) {
+    errorMessage.innerText = 'Thank you! We will get back to you soon!';
+    errorMessage.className = 'alert success';
+    setTimeout(() => {
+        form.submit();
+      },
+      5000);
+  }
+  return true;
+}
+form.addEventListener('submit', (event) => {
+  if (checkEmail(email.value) === false) {
+    event.preventDefault();
+  } else {
+    event.run();
+  }
+});
