ad1da8cf968f1b012bdb08a073ce50dc0b7a6413
iAmWaris97 <haleem.waris@gmail.com>
2023-04-23 19:45:12 +0430

    add form validation

--- index.js
+++ index.js
@@ -42,8 +42,7 @@ window.onload = () => {
     popUp.classList.add('hidden');
   });
 
-  const projectsData = [
-    {
+  const projectsData = [{
       title: 'Private Investigators',
       subtitle: 'Find the Truth, Book a P.I.',
       techStack: 'Full Stack Dev',
@@ -170,4 +169,114 @@ window.onload = () => {
     popupContainer.append(mobilePopupElement);
   });
 
+  const form = document.querySelector('#form');
+  const messageContainer = document.querySelector('.message');
+
+  const isValidEmail = (email) => {
+    const eamilPattern = /^(([^<>()[\]\\.,;:\s@"A-Z]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+    return eamilPattern.test(email);
+  };
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const valid = isValidEmail(formData.get('email'));
+    if (valid) {
+      const message = document.createElement('span');
+      message.classList.add('success');
+      message.innerHTML = 'Data sent successfully! 🙌';
+      messageContainer.replaceChildren(message);
+    } else {
+      const message = document.createElement('span');
+      message.classList.add('error');
+      message.innerHTML = 'Error!⚡ Incorrect format. Enter email in lowercase';
+      messageContainer.replaceChildren(message);
+    }
+  });
+
+  const name = document.querySelector('#name');
+  const email = document.querySelector('#email');
+  const comment = document.querySelector('#mesasge');
+
+  function saveData() {
+    const data = {
+      fieldName: name.value,
+      fieldEmail: email.value,
+      fieldComment: comment.value,
+    };
+    localStorage.setItem('data', JSON.stringify(data));
+  }
+
+  let formObject = JSON.parse(localStorage.getItem('data'));
+  if (!formObject) {
+    formObject = {
+      name: '',
+      email: '',
+      comment: '',
+    };
+    saveData();
+  }
+
+  if (!formObject.name) {
+    formObject.name = '';
+  }
+
+  if (!formObject.email) {
+    formObject.email = '';
+  }
+
+  if (!formObject.comment) {
+    formObject.comment = '';
+  }
+
+  name.value = formObject.name;
+  name.addEventListener('change', (e) => {
+    formObject.name = e.target.value;
+    localStorage.setItem('data', JSON.stringify(formObject));
+  });
+
+  email.value = formObject.email;
+  email.addEventListener('change', (e) => {
+    formObject.email = e.target.value;
+    localStorage.setItem('data', JSON.stringify(formObject));
+  });
+
+  comment.value = formObject.comment;
+  comment.addEventListener('change', (e) => {
+    formObject.comment = e.target.value;
+    localStorage.setItem('data', JSON.stringify(formObject));
+  });
 };
+
+// Wait for the page to load
+document.addEventListener('DOMContentLoaded', () => {
+  // Get a reference to the portfolio grid
+  const portfolioGrid = document.querySelector('.recent-work');
+
+  // Check if the user has scrolled down to the portfolio grid
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.bottom >= 0 &&
+      rect.left <= (window.innerWidth || document.documentElement.clientWidth) &&
+      rect.right >= 0);
+  }
+
+  window.addEventListener('scroll', () => {
+    // Get all child elements of the portfolio grid
+    const portfolioItems = portfolioGrid.querySelectorAll('.card');
+
+    // Loop through each child element and check if it's in the viewport
+    portfolioItems.forEach((portfolioItem) => {
+      if (isElementInViewport(portfolioItem) && !portfolioItem.classList.contains('animate__animated')) {
+        // Add the 'animated' and 'fade-in' classes to the child element to trigger the animation
+        portfolioItem.classList.add('animate__animated', 'animate__fadeIn');
+      } else if (!isElementInViewport(portfolioItem) && portfolioItem.classList.contains('animate__animated')) {
+        // Remove the 'animated' class from the child element when it goes out of view
+        portfolioItem.classList.remove('animate__animated');
+        portfolioItem.classList.remove('animate__fadeIn');
+      }
+    });
+  });
+});
