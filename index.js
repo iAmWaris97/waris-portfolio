487734172777a24dfd87b49f2388db32c69ecdd5
iAmWaris97 <haleem.waris@gmail.com>
2023-04-23 16:26:31 +0430

    create variable to act on window load

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-const navItem1 = document.querySelector('.item1');
-const navItem2 = document.querySelector('.item2');
-const navItem3 = document.querySelector('.item3');
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
-
-navItem1.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
-
-navItem2.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
-
-navItem3.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
+window.onload = () => {
+  const barMenu = document.querySelector('.bar-menu');
+  const popUp = document.querySelector('.pop-up');
+  const close = document.querySelector('.close');
+  const menu = document.querySelector('.menu');
+  const skillItems = document.querySelectorAll('.skill-item');
+};
