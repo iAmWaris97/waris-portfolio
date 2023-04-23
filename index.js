cd122e417f12ce3d1406d1fee04ec94f2b0393a6
iAmWaris97 <haleem.waris@gmail.com>
2023-04-23 16:31:37 +0430

    add event listener on skills to open and close flyers

--- index.js
+++ index.js
@@ -4,4 +4,29 @@ window.onload = () => {
   const close = document.querySelector('.close');
   const menu = document.querySelector('.menu');
   const skillItems = document.querySelectorAll('.skill-item');
+
+  // On click add active class to clicked skills list item and remove from others
+  skillItems.forEach((skillItem) => {
+    skillItem.addEventListener('click', () => {
+      // if item is already active remove active class
+      if (skillItem.classList.contains('active')) {
+        skillItem.classList.remove('active');
+        const icon = skillItem.querySelector('.item-1-header > i');
+        icon.classList.remove('fa-solid', 'fa-angle-down');
+        icon.classList.add('fa-solid', 'fa-angle-right');
+        return;
+      }
+      skillItems.forEach((item) => {
+        // Change item-1-header > i to fa-solid fa-angle-right next arrow"
+        const icon = item.querySelector('.item-1-header > i');
+        icon.classList.remove('fa-solid', 'fa-angle-down');
+        icon.classList.add('fa-solid', 'fa-angle-right');
+        item.classList.remove('active');
+      });
+      skillItem.classList.add('active');
+      const icon = skillItem.querySelector('.item-1-header > i');
+      icon.classList.remove('fa-solid', 'fa-angle-right');
+      icon.classList.add('fa-solid', 'fa-angle-down');
+    });
+  });
 };
