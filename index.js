2dbd24f461d1f7e791b24e20031c34ff02bff644
iAmWaris97 <haleem.waris@gmail.com>
2023-04-23 19:31:18 +0430

    Map the projects in portfolio section

--- index.js
+++ index.js
@@ -93,4 +93,81 @@ window.onload = () => {
     },
   ];
 
+  const projectCardString = projectsData.map((project) => `
+    <div class="card grid__item ">
+      <div class="inner-card">
+        <div class="card-img card-1">
+          <img
+            src="${project.src}"
+            alt="Project Snapshot"
+          />
+        </div>
+        <div class="desc">
+          <h3>${project.title}</h3>
+          <h4>${project.subtitle} <span>${project.techStack}</span> <span>${project.year}</span></h4>
+          <p>${project.highlight}</p>
+          <ul>
+            ${project.lang.map((tech) => `<li>${tech}</li>`).join('')}
+          </ul>
+          <button type="button" class="project-btn button">
+            See Project
+          </button>
+        </div>
+      </div>
+    </div>
+  `);
+
+  const popupContainer = document.querySelector('.project-popup-container');
+  const recentWork = document.querySelector('.recent-work');
+  const parser = new DOMParser();
+
+  projectCardString.forEach((projectString, index) => {
+    const projectElement = parser.parseFromString(projectString, 'text/html').body.firstChild;
+
+    const mobilePopup = `
+      <div class="project-popup-hidden ">
+        <div class="project-background"></div>
+          <div class="project-popup">
+
+            <div class="project-header">
+              <i class="fa-solid fa-xmark project-close"></i>
+              <h2>${projectsData[index].title}</h2>
+              <p>${projectsData[index].subtitle} <span>${projectsData[index].techStack}</span> <span>${projectsData[index].year}</span></p>
+            </div>
+            <div class="project-img" style="background-image: url(${projectsData[index].src})">
+            </div>
+            <div class="project-footer">
+              <p class="project-desc">${projectsData[index].description}</p>
+              <div>
+                <ul>
+                  ${projectsData[index].lang.map((tech) => `<li>${tech}</li>`).join('')}
+                </ul>
+                <span></span>
+                <div class="project-links">
+                  <button ><a href="${projectsData[index].live}" target="_blank">See Live <i class="fa-solid fa-arrow-up-right-from-square"></i></a></button>
+                  <button><a href="${projectsData[index].source}" target="_blank">See Source <i class="fa-brands fa-github"></i></a></button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    `;
+
+    const mobilePopupElement = parser.parseFromString(mobilePopup, 'text/html').body.firstChild;
+
+    const projectBtn = projectElement.querySelector('.project-btn');
+    const closeBtn = mobilePopupElement.querySelector('.project-close');
+
+    projectBtn.addEventListener('click', () => {
+      mobilePopupElement.classList.toggle('project-popup-hidden');
+    });
+
+    closeBtn.addEventListener('click', () => {
+      mobilePopupElement.classList.toggle('project-popup-hidden');
+    });
+
+    recentWork.append(projectElement);
+    popupContainer.append(mobilePopupElement);
+  });
+
 };
